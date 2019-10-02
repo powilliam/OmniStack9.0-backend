b314537cabd572eced7355f212d3fd83abff3ba5
powilliam <powilliam19@gmail.com>
2019-10-02 11:51:03 -0400

    server -> add: use cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const Routes = require('./routes');
 
 const dataBase = require('./mongoose');
 
 const app = express();
+app.use(cors());
 
 dataBase.connectToMongoServer("mongodb+srv://customuser:senha@heaven-uc9c7.mongodb.net/semana09?retryWrites=true&w=majority");
 
